b7d59063768474d829c2428bb9e31fe9a028e269
pranish9 <pranishtuladhar9@gmail.com>
2026-05-27 06:31:21 +0545

    changes

--- patronNP-project/src/App.jsx
+++ patronNP-project/src/App.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from 'react';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
+import Testimonials from './components/Testimonials';
+import CTA from './components/CTA';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div className="min-h-screen flex 
-    flex-col bg-gray-50">
-     <Navbar />
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      <Hero />
+      <Features />
+      <HowItWorks />
+      <Testimonials />
+      <CTA />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
